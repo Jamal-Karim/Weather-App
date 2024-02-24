a98e9081faa10b78128ba88d09503f387895ae06
Jamal Karim <1jamalkarim@gmail.com>
2024-02-24 15:13:10 -0800

    integrate webpack and add starter code for api fetching

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log("hello world"),async function(){const o=await fetch("http://api.weatherapi.com/v1/forecast.json?key=9da97e80930143a38c213928241202&q=corona&days=3"),a=await o.json();console.log(a)}();
