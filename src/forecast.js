e2bdb4cb036eae0a4c017c203b9efdb988226fa4
Jamal Karim <1jamalkarim@gmail.com>
2024-02-25 19:04:16 -0800

    add time formatting logic

--- src/forecast.js
+++ src/forecast.js
@@ -14,13 +14,26 @@ export async function getForecastData(data){
         if(hrsArr[i] > 23){
             const {temp_f, temp_c, condition, time} = data.forecast.forecastday[1].hour[hrsArr[i] - 23];
             const {text} = condition;
-            const splicedTime = time.slice(11,16);
+            //want format "1:00 PM or 10:00 AM"
+            // const splicedTime = time.slice(11,16);
+            const militaryTime = parseInt(time.slice(11,13));
             forecast[hrsArr[i]] = {temp_f, temp_c, text, splicedTime};
         } else {
             const {temp_f, temp_c, condition, time} = data.forecast.forecastday[0].hour[hrsArr[i]];
             const {text} = condition;
-            const splicedTime = time.slice(11,16);
-            forecast[hrsArr[i]] = {temp_f, temp_c, text, splicedTime};
+            // const splicedTime = time.slice(11,16);
+            let updatedTime = parseInt(time.slice(11,13));
+            if(updatedTime === 0){
+                updatedTime = '12:00 AM';
+            } else if (updatedTime <= 11){
+                updatedTime = `${updatedTime}:00 AM`;
+            } else if (updatedTime === 12 ){
+                updatedTime = '12:00 PM';
+            } else{
+                updatedTime -= 12;
+                updatedTime = `${updatedTime}:00 PM`;
+            }
+            forecast[hrsArr[i]] = {temp_f, temp_c, text, updatedTime};
         }
     }
 
