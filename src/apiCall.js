0b68bf2c2ebcdff0eb898a591016095dc552d6ca
Jamal Karim <1jamalkarim@gmail.com>
2024-03-09 21:51:17 -0800

    modularize code to clean up index.js

---
+++ src/apiCall.js
@@ -0,0 +1,66 @@
+import { getLocationData } from "./location.js";
+import { getTempData } from "./temp.js";
+import { getForecastData } from "./forecast.js";
+import { loadWeatherUi } from "./loadUi.js";
+
+let dataObj;
+export { dataObj };
+const body = document.querySelector("body");
+
+async function getData(city) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=9da97e80930143a38c213928241202&q=${city}&days=3`
+    );
+
+    if (!response.ok) {
+      userCity.value = "";
+      userCity.setAttribute("placeholder", "Please enter valid city");
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching the data:", error);
+    throw error;
+  }
+}
+
+export async function combinedData(city) {
+  try {
+    const apiCall = await getData(city);
+    const location = await getLocationData(apiCall);
+    const tempData = await getTempData(apiCall);
+
+    const forecast = await getForecastData(apiCall);
+    console.log(forecast);
+
+    const data = { ...location, ...tempData, ...forecast };
+
+    console.log(data);
+    dataObj = data;
+    console.log(dataObj.forecast[Object.keys(dataObj.forecast)[0]]);
+    body.appendChild(
+      loadWeatherUi(
+        data.condition,
+        data.tempF,
+        data.tempC,
+        data.location,
+        data.condition,
+        data.sunrise,
+        data.sunset,
+        data.humidity,
+        data.wind_mph,
+        data.uv,
+        data.forecast
+      )
+    );
+  } catch (error) {
+    console.error("Error combining data:", error);
+    throw error;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import { getLocationData } from "./location.js";
-import { getTempData } from "./temp.js";
-import { getForecastData } from "./forecast.js";
-import { loadWeatherUi } from "./loadUi.js";
 import { handleCurrentTemp } from "./buttons.js";
+import { combinedData, dataObj } from "./apiCall.js";
 
 import "./styles/main.css";
 
@@ -13,8 +10,6 @@ const searchIcon = document.querySelector(".searchIcon");
 let onCelsius = false;
 export { onCelsius };
 
-let dataObj;
-
 userCity.addEventListener("focus", () => {
   userCity.removeAttribute("placeholder");
 });
@@ -24,64 +19,6 @@ userCity.addEventListener("blur", () => {
     userCity.setAttribute("placeholder", "Enter a city to get started!");
   }
 });
-
-export async function getData(city) {
-  try {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=9da97e80930143a38c213928241202&q=${city}&days=3`
-    );
-
-    if (!response.ok) {
-      userCity.value = "";
-      userCity.setAttribute("placeholder", "Please enter valid city");
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await response.json();
-
-    console.log(data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching the data:", error);
-    throw error;
-  }
-}
-
-async function combinedData(city) {
-  try {
-    const apiCall = await getData(city);
-    const location = await getLocationData(apiCall);
-    const tempData = await getTempData(apiCall);
-
-    const forecast = await getForecastData(apiCall);
-    console.log(forecast);
-
-    const data = { ...location, ...tempData, ...forecast };
-
-    console.log(data);
-    dataObj = data;
-    console.log(dataObj.forecast[Object.keys(dataObj.forecast)[0]]);
-    body.appendChild(
-      loadWeatherUi(
-        data.condition,
-        data.tempF,
-        data.tempC,
-        data.location,
-        data.condition,
-        data.sunrise,
-        data.sunset,
-        data.humidity,
-        data.wind_mph,
-        data.uv,
-        data.forecast
-      )
-    );
-  } catch (error) {
-    console.error("Error combining data:", error);
-    throw error;
-  }
-}
 
 //landing page for loading first weather ui
 searchIcon.addEventListener("click", async (event) => {
